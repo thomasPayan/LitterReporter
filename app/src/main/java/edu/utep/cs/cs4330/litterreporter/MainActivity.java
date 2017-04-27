1f48f51b7a868c6c476dd2eecf5bbbfda06ce177
thomasPayan <thomaspayan91@gmail.com>
2017-04-27 15:33:36 -0600

    got location and pic to work

--- app/src/main/java/edu/utep/cs/cs4330/litterreporter/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/litterreporter/MainActivity.java
@@ -1,6 +1,17 @@
 package edu.utep.cs.cs4330.litterreporter;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,35 +19,99 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener{
+    double longi;
+    double lati;
+    Geocoder g;
+    List<Address> addresses;
+    Button cont;
+    Button location;
+    EditText city;
+    EditText state;
+    EditText address;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button cont=(Button)findViewById(R.id.cont);
-        final EditText city=(EditText)findViewById(R.id.city);
-        final EditText state=(EditText)findViewById(R.id.state);
-        final EditText address=(EditText)findViewById(R.id.address);
+        cont = (Button) findViewById(R.id.cont);
+        location = (Button) findViewById(R.id.location);
+        city = (EditText) findViewById(R.id.city);
+        state = (EditText) findViewById(R.id.state);
+        address = (EditText) findViewById(R.id.address);
+        g = new Geocoder(MainActivity.this, Locale.getDefault());
         cont.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(city!=null&&state!=null&&address!=null) {
+                if (!city.getText().toString().equals("") && !state.getText().toString().equals("") && !address.getText().toString().equals("")) {
                     Intent i = new Intent(MainActivity.this, Report.class);
                     Bundle extras = new Bundle();
                     extras.putString("city", city.getText().toString());
                     extras.putString("state", state.getText().toString());
-                    extras.putString("address",address.getText().toString());
+                    extras.putString("address", address.getText().toString());
                     i.putExtras(extras);
                     startActivity(i);
-                }else{
-                   toast("Fields incomplete");
+                } else {
+                    toast("Fields incomplete");
                 }
             }
 
-    });
+        });
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, MainActivity.this);
+                    longi = location.getLongitude();
+                    lati = location.getLatitude();
+                }catch (SecurityException e){
+                    toast("no permissions");
+                }
+                try {
+                    addresses = g.getFromLocation(lati, longi, 1);
+                    String addr = addresses.get(0).getAddressLine(0);
+                    String ct = addresses.get(0).getLocality();
+                    String st = addresses.get(0).getAdminArea();
+                    address.setText(addr);
+                    state.setText(st);
+                    city.setText(ct);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+        });
     }
     protected void toast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
--- app/src/main/java/edu/utep/cs/cs4330/litterreporter/Report.java
+++ app/src/main/java/edu/utep/cs/cs4330/litterreporter/Report.java
@@ -1,6 +1,9 @@
 package edu.utep.cs.cs4330.litterreporter;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
 public class Report extends AppCompatActivity {
@@ -19,7 +24,12 @@ public class Report extends AppCompatActivity {
     EditText summary;
     EditText name;
     EditText phone;
-    Random r;
+    Random random;
+    Button capture;
+    File pic;
+    boolean isPicture;
+    String picName;
+    String directory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +37,14 @@ public class Report extends AppCompatActivity {
         setContentView(R.layout.activity_report);
         Button submit=(Button)findViewById(R.id.submit);
         Button cancel=(Button)findViewById(R.id.cancel);
+        capture=(Button)findViewById(R.id.capture);
         summary=(EditText)findViewById(R.id.summary);
         name=(EditText)findViewById(R.id.name);
         phone=(EditText)findViewById(R.id.phone);
         Bundle extras = getIntent().getExtras();
-        r=new Random();
+        isPicture=false;
+        directory="/storage/3631-3663/DCIM/Camera/";
+        random=new Random();
 
         if (extras != null) {
             city = extras.getString("city");
@@ -42,13 +55,19 @@ public class Report extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(summary.getText().toString()!=null&&name.getText().toString()!=null&&phone.getText().toString()!=null) {
-                    id= Integer.toString(r.nextInt(10000-1000)+1000);
+                    id= Integer.toString(random.nextInt(10000-1000)+1000);
                     body="A report has been filed at "+city+" "+state+" "+address+"\n"+"Desctription:\n"+summary.getText().toString()+"\n"+"Call back info:\nNumber :"+phone.getText().toString()+"  "+"Name: "+name.getText().toString()+"\n\nID:"+id;
                     Intent i = new Intent(Intent.ACTION_SEND);
-                    i.setType("message/rfc822");
+                    //i.setType("message/rfc822");
+                    i.setType("applicatin/image");
                     i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"thomaspayan91@gmail.com"});
                     i.putExtra(Intent.EXTRA_SUBJECT, "Litter Report id:"+id);
                     i.putExtra(Intent.EXTRA_TEXT   , body);
+                    if(isPicture) {
+                        String filePath=pic.getAbsolutePath();
+                        Uri uPath=Uri.parse("file://"+filePath);
+                        i.putExtra(Intent.EXTRA_STREAM, uPath);
+                    }
                     try {
                         startActivity(Intent.createChooser(i, "Report Sent"));
 
@@ -58,6 +77,18 @@ public class Report extends AppCompatActivity {
                }else{
                     toast("Fields incomplete");
                 }
+            }
+
+        });
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    captureIncident();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                isPicture=true;
             }
 
         });
@@ -71,5 +102,15 @@ public class Report extends AppCompatActivity {
     }
     protected void toast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void captureIncident() throws IOException {
+        picName=Integer.toString(random.nextInt(10000-1000)+1000);
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES) ;
+        pic =  File.createTempFile(picName,".jpg",dir);
+        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile(pic));
+        startActivityForResult(i,1);
+
     }
 }
